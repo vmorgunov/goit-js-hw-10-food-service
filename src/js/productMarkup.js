8cfcd81d3f90d48a9794707698eaf6d4718a4e71
Vitalii Morgunov <utlab13@gmail.com>
2021-06-24 15:47:49 +0200

    Add material icons package

--- src/js/productMarkup.js
+++ src/js/productMarkup.js
@@ -1,5 +1,6 @@
 import product from '../templates/product.hbs';
 import menu from '../menu.json';
+import 'material-icons/iconfont/material-icons.css';
 const productList = document.querySelector('.js-menu');
 const markup = createProductMarkup(menu);
 
